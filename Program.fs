a016d543362725838221880b37e051620b31b10c
Joseph Price <pricejosephd@gmail.com>
2024-07-11 17:57:51 -0400

    add native example

--- Program.fs
+++ Program.fs
@@ -1,9 +1,16 @@
-﻿open Fable.Core
+﻿#if FABLE_COMPILER
+open Fable.Core
 open Thoth.Json
+#else
+open Thoth.Json.Net
+#endif
 
-// NOTE: with StringEnum, below prints "success: Invalid"
-//       without StringEnum, it prints "The following `failure` occurred with the decoder: Cannot find case Invalid in Program.State"
+// NOTE: With StringEnum, below prints "success: Invalid"
+//       Without StringEnum, it prints "The following `failure` occurred with the decoder: Cannot find case Invalid in Program.State"
+//       Running with `dotnet run` gives the same result
+#if FABLE_COMPILER
 [<StringEnum>]
+#endif
 type State =
     | On
     | Off
