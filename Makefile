e2eea7ac9926f4edc17d914d10c98ecea06688a3
Joseph Price <pricejosephd@gmail.com>
2025-02-21 13:50:22 -0500

    test newer

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 watch:
 	dotnet fable watch --runWatch node Program.fs.js
+
+watch-release:
+	dotnet fable watch -c Release --runWatch node Program.fs.js 
--- Program.fs
+++ Program.fs
@@ -1,19 +1,12 @@
-module rec SerializationTests
+module Program
 
 type Codec<'a> = | Codec
 
 type Codecs =
     // at least two overloads are required here to avoid ambiguity
-    static member Codecs(_output: int, _mthd: Codecs) : Codec<int> = Codec
-    static member Codecs(_output: bool, _mthd: Codecs) : Codec<bool> = Codec
+    static member inline Codecs(_output: int, _mthd: Codecs) : Codec<int> = Codec
+    static member inline Codecs(_output: bool, _mthd: Codecs) : Codec<bool> = Codec
 
-    static member inline Codecs< ^a, ^b
-        when (^a): (static member codecInner: Codec< ^b > -> Codec< ^a >)
-        and (Codecs or ^b): (static member Codecs: ^b * Codecs -> Codec< ^b >)>
-        (_output: ^a, _mthd: Codecs)
-        : Codec< ^a > =
-        let b: Codec< ^b > = Codecs.Invoke()
-        Codec
 
     static member inline Invoke() =
         let inline call_2 (a: ^a, b: ^b) =
@@ -24,7 +17,22 @@ type Codecs =
 
         call Unchecked.defaultof<Codecs>
 
-let inline getCodec<'a, 'b
+type Codecs with
+    static member inline Codecs< ^a, ^b
+        when (^a): (static member codecInner: Codec< ^b > -> Codec< ^a >)
+        and (Codecs or ^b): (static member Codecs: ^b * Codecs -> Codec< ^b >)>
+        (_output: ^a, _mthd: Codecs)
+        : Codec< ^a > =
+        // let x = Codecs.Invoke()
+        // this line gets inlined without scope handling (Identifier 'x_1' has already been declared)
+        let x = 1
+        'a.codecInner (Codecs.Invoke())
+
+let inline getCodec<'a when (Codecs or ^a): (static member Codecs: ^a * Codecs -> Codec< ^a >)> () : Codec< ^a > =
+    let a: Codec<'a> = Codecs.Invoke()
+    a
+
+let inline getCodecs<'a, 'b
     when (Codecs or ^a): (static member Codecs: ^a * Codecs -> Codec< ^a >)
     and (Codecs or ^b): (static member Codecs: ^b * Codecs -> Codec< ^b >)>
     ()
@@ -36,6 +44,16 @@ let inline getCodec<'a, 'b
 type Container<'T> =
     | Container
 
-    static member codecInner<'T, 'E>(t: Codec<'T>) : Codec<Container<'T>> = Codec
+    static member codecInner<'T, 'E>(t: Codec<'T>) : Codec<Container<'T>> =
+        let y = 1
+        Codec
 
-let _: (Codec<Container<int>> * Codec<Container<int>>) = getCodec ()
+// single container works
+let _: Codec<Container<int>> = getCodec ()
+
+// pais works
+let _: (Codec<int> * Codec<int>) = getCodecs ()
+
+// pair of containers don't generates duplciate code in debug mode
+// SyntaxError: Identifier 'x_1' has already been declared
+let _: (Codec<Container<int>> * Codec<Container<int>>) = getCodecs ()
